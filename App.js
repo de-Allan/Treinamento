096eac28e44accf14e9ca0150110bd400a835ed7
deAllan <95139882+de-Allan@users.noreply.github.com>
2022-08-10 21:39:38 -0300

    primeiro

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react"
+import { Alert, SafeAreaView, StyleSheet, Text, TouchableOpacity, ActivityIndicator } from "react-native"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+
+const [k, setk] = useState(false)
+
+function chave () {
+
+    setk(!k)
+
+}
+
+    return (
+        <SafeAreaView style={styles.container}>
+
+            <TouchableOpacity
+                style={styles.Button}
+                onPress={chave}
+            >
+                <Text style={styles.pesquisa}>Clique aqui</Text>
+
+            </TouchableOpacity>
+
+            {
+                k === true ?
+                <Text style={{color:'green'}}>O botao esta true </Text>
+                : 
+                <Text style={{color: 'red'}}>O botao esta false</Text>
+            }
+
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        marginHorizontal: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFF'
+    },
+    pesquisa: {
+        color: 'white'
+    },
+
+    Button: {
+        marginTop: 30,
+        width: 100,
+        backgroundColor: 'blue',
+        borderRadius: 4,
+        height: 35,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+})
+
+export default App
